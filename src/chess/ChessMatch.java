caafb2ac977f2788373d75fd6279ecfec4e630d7
jcheavy <jcheavy@hotmail.com>
2020-06-26 22:44:54 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -89,8 +88,7 @@ public class ChessMatch {
 		}
 		ChessPiece movedPiece = (ChessPiece) board.piece(target);
         
-		// especial move promotion
-		
+		// especial move promotion		
 		promoted = null;
 		if(movedPiece instanceof Pawn) {
 			if((movedPiece.getColor() == Color.WHITE && target.getRow() == 0) || (movedPiece.getColor() == Color.BLACK && target.getRow() == 7)   ) {
@@ -126,7 +124,7 @@ public class ChessMatch {
 		}
 		
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q") ) {
-			throw new InvalidParameterException("Invalid type for promotion ");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
